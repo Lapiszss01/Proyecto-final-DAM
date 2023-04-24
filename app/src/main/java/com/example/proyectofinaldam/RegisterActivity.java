8314d4db3c4435d44613578ef91cd7747093e271
Jonathan <jonathan.rbm01@gmail.com>
2023-04-24 11:29:38 +0200

    Vista registro añadido

--- app/src/main/java/com/example/proyectofinaldam/RegisterActivity.java
+++ app/src/main/java/com/example/proyectofinaldam/RegisterActivity.java
@@ -1,17 +1,15 @@
 package com.example.proyectofinaldam;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-
 
 }
