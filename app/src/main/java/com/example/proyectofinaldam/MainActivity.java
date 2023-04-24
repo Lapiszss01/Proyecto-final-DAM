8d99a4511cd3132cd1f18f2edd8c24e3d5ccff1a
Jonathan <jonathan.rbm01@gmail.com>
2023-04-24 12:06:20 +0200

    Añadidos a la vista register

--- app/src/main/java/com/example/proyectofinaldam/MainActivity.java
+++ app/src/main/java/com/example/proyectofinaldam/MainActivity.java
@@ -2,16 +2,36 @@ package com.example.proyectofinaldam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnRegisto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnRegisto = (Button) findViewById(R.id.btnRegister);
+        btnRegisto.setOnClickListener(this);
+
+
     }
 
+    public void lanzarRegistro(){
+        Intent i = new Intent(MainActivity.this,RegisterActivity.class);
+        startActivity(i);
+    }
 
-
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnRegister:
+                lanzarRegistro();
+        }
+    }
 }
