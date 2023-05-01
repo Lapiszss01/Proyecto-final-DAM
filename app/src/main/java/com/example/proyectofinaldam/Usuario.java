7e838295f01ced2061496d3fc954ea2bc62218ef
Jonathan <jonathan.rbm01@gmail.com>
2023-05-01 15:41:56 +0200

    Empezado código login

---
+++ app/src/main/java/com/example/proyectofinaldam/Usuario.java
@@ -0,0 +1,67 @@
+package com.example.proyectofinaldam;
+
+public class Usuario {
+    int id;
+    String nombre, apellidos, usuario, password;
+
+    public Usuario(){
+
+    }
+
+    public boolean isNull(){
+        if(nombre.equals("")&&apellidos.equals("")&&usuario.equals("")&&password.equals("")){
+            return false;
+        } else {return true;}
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", usuario='" + usuario + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ app/src/main/java/com/example/proyectofinaldam/datosUsuario.java
@@ -0,0 +1,72 @@
+package com.example.proyectofinaldam;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+
+public class datosUsuario {
+
+    Context c;
+    Usuario u;
+    ArrayList<Usuario> lista;
+
+    SQLiteDatabase sql;
+    String bd = "BDUsuarios";
+    String tabla = "create table usuario(id integer primary key autoincrement, usuario text, pass text, nombre text, ap text)";
+
+    public datosUsuario(Context c){
+        this.c=c;
+        sql = c.openOrCreateDatabase(bd,c.MODE_PRIVATE,null);
+        sql.execSQL(tabla);
+        u = new Usuario();
+    }
+
+    public boolean insertUsuario(Usuario u){
+        if(buscar(u.getUsuario())==0){
+            ContentValues cv = new ContentValues();
+            cv.put("usuario",u.getUsuario());
+            cv.put("pass",u.getPassword());
+            cv.put("nombre",u.getNombre());
+            cv.put("ap",u.getApellidos());
+            return (sql.insert("usuario",null,cv)>0);
+        }else {return false;}
+    }
+
+    private int buscar(String u) {
+        int x=0;
+        lista = selectUsuario();
+        for (Usuario us:lista){
+
+            if(us.getUsuario().equals(u)){
+                x++;
+            }
+
+        }
+
+        return x;
+    }
+
+    public ArrayList<Usuario> selectUsuario(){
+        ArrayList<Usuario> lista = new ArrayList<Usuario>();
+        lista.clear();
+        Cursor cursor = sql.rawQuery("select * from usuario",null);
+        if (cursor!=null&&cursor.moveToFirst()){
+            do {
+
+                Usuario u = new Usuario();
+                u.setId(cursor.getInt(0));
+                u.setUsuario(cursor.getString(1));
+                u.setPassword(cursor.getString(2));
+                u.setNombre(cursor.getString(3));
+                u.setApellidos(cursor.getString(4));
+                lista.add(u);
+
+            }while (cursor.moveToNext());
+        }
+        return lista;
+    }
+
+}
