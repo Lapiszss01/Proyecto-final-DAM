904177190bc685bb1d6ba9b32fd7ad94d0afbf7d
jonathanbetancourtdD <jonathan@digitaldot.es>
2023-05-04 13:49:37 +0200

    Principio del menu creado

--- app/src/main/java/com/example/proyectofinaldam/ui/MainHubActivity.java
+++ app/src/main/java/com/example/proyectofinaldam/ui/MainHubActivity.java
@@ -3,13 +3,18 @@ package com.example.proyectofinaldam.ui;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.proyectofinaldam.R;
 import com.example.proyectofinaldam.data.model.Usuario;
 import com.example.proyectofinaldam.data.sql.DatosUsuario;
+import com.example.proyectofinaldam.databinding.ActivityMainBinding;
+import com.example.proyectofinaldam.databinding.ActivityMainHubBinding;
 
 public class MainHubActivity extends AppCompatActivity {
+
+    @NonNull ActivityMainHubBinding binding;
 
     int id = 0;
     Usuario u;
@@ -18,7 +23,11 @@ public class MainHubActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_hub);
+        binding = ActivityMainHubBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //setContentView(R.layout.activity_main_hub);
+
+
 
         Bundle b = getIntent().getExtras();
         id = b.getInt("Id");
