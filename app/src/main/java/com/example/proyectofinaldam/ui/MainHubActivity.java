c93c648c64d24af8ae8a3d1b547cb8342f07e0d7
jonathanbetancourtdD <jonathan@digitaldot.es>
2023-05-05 10:26:16 +0200

    Menu principal funciona

--- app/src/main/java/com/example/proyectofinaldam/ui/MainHubActivity.java
+++ app/src/main/java/com/example/proyectofinaldam/ui/MainHubActivity.java
@@ -5,6 +5,9 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.proyectofinaldam.R;
 import com.example.proyectofinaldam.data.model.Usuario;
@@ -25,17 +28,44 @@ public class MainHubActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainHubBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        //setContentView(R.layout.activity_main_hub);
-
-
+        replaceFragment(new EjercicioFragment());
 
         Bundle b = getIntent().getExtras();
         id = b.getInt("Id");
         dao = new DatosUsuario(this);
         u = dao.getUsuarioById(id);
-
         Log.d("Funciona?",""+u.toString());
 
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.menuEj:
+                    replaceFragment(new EjercicioFragment());
+                    break;
+                case R.id.menuAlim:
+                    replaceFragment(new AlimentoFragment());
+                    break;
+                case R.id.menuPerf:
+                    replaceFragment(new PerfilFragment());
+                    break;
+                case R.id.menuPref:
+                    replaceFragment(new ConfiguracionFragment());
+                    break;
+
+            }
+
+            return true;
+        });
+
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
     }
 
 }
